8050927462e9b6f7a1101647ebdd072b3d9c0390
Jinx <linnlindgren03@gmail.com>
2023-11-01 17:43:36 +0100

    change 1.0

--- src/exercises/ex2arrays/Arrays.java
+++ src/exercises/ex2arrays/Arrays.java
@@ -1,45 +1,73 @@
 package exercises.ex2arrays;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class Arrays {
 
     /* The input functions */
+    public static final Scanner input = new Scanner(in);
+
     public static int[] getArrayOfIntsFromUser() {
-        // TODO
-        return new int[5];
+        out.println("Give me 5 ints, write all numbers in one line with a whitespace in between: ");
+        String ord = input.nextLine(); //1 2 3 5 6
+        String[] arr = ord.split(" ");//this will return an array, split will always return an array
+        int[] iar = new int[5];
+
+        for (int el = 0; el < 5; el++) {
+            iar[el] = Integer.parseInt(arr[el]);
+        }
+
+        return iar;
     }
 
     public static int getValueFromUser() {
-        // TODO
-        return 0;
+        out.println("Insert a value to find: ");
+        int value = input.nextInt();
+
+        return value;
     }
 
     /* The output functions */
     public static void printIndexToUser(int the_index, int the_value) {
-        // TODO
+        out.println("Value: " + String.valueOf(the_value) + "is at index: " + String.valueOf(the_index));
+
     }
 
     public static void printNotFoundToUser(int the_value) {
-        // TODO
+        out.println("Value: " + String.valueOf(the_value) + " is not present in list");
     }
 
     public static void printResultToUser(int the_index, int the_value) {
-        // TODO
+        if (the_index == -1) {
+            printNotFoundToUser(the_value);
+        }
+        else {
+            printIndexToUser(the_index, the_value);
+        }
     }
 
 
     /* The Algorithm */
     public static int findIndexOfValue(int[] a_list, int a_value) {
-        // TODO
-        return 0;
+        for (int el = 0; el < a_list.length; el++) {
+            if (a_value == a_list[el]) {
+                return el;
+            }
+        }
+        return -1;
     }
+
 
     /* The top-level behavior */
     public static void listBasicsProgram() {
         // INPUT
-        int[] theList = getArrayOfIntsFromUser();
-        int theValue = getValueFromUser();
+        int[] theList = getArrayOfIntsFromUser(); //iar
+        int theValue = getValueFromUser();  //value
         // PROCESS
-        int theIndex = findIndexOfValue(theList, theValue);
+        int theIndex = findIndexOfValue(theList, theValue); // done
         // OUTPUT
         printResultToUser(theIndex, theValue);
     }
